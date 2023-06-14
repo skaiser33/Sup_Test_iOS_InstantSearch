353aae84ba73b566304eef1d164d956b54488724
Steven <skaiser33@gmail.com>
2023-06-14 15:58:58 -0400

    Initial Commit

---
+++ Sup_Test_iOS_InstantSearch/Sup_Test_iOS_InstantSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sup_Test_iOS_InstantSearchApp.swift
+//  Sup_Test_iOS_InstantSearch
+//
+//  Created by Steven Kaiser on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Sup_Test_iOS_InstantSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
