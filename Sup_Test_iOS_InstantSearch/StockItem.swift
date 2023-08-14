2a220f1baadb02b97ea2e5ef8f430b755285c4b0
skaiser33 <skaiser33@gmail.com>
2023-08-14 09:28:09 -0400

    cleanup

--- Sup_Test_iOS_InstantSearch/StockItem.swift
+++ Sup_Test_iOS_InstantSearch/StockItem.swift
@@ -1,8 +1,12 @@
-/*Defines a structure that represent a record in your index.
+/*
+ Defines a structure that represent a record in your index.
  
  For simplicity’s sake, the structure only provides the name of the product.
  
- It must conform to the Codable protocol to work with InstantSearch.*/
+ It must conform to the Codable protocol to work with InstantSearch.
+ 
+ You can add additionalal attributes based on the attributes in your Algolia index's records.
+ */
 
 struct StockItem: Codable {
   let name: String
